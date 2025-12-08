b890a7df58928a2d27b1ba7708d4332b6d430262
Gere Attila <gereattila98@gmail.com>
2025-12-09 00:30:46 +0200

    simpler exception handler

--- src/APIExceptionHandler.php
+++ src/APIExceptionHandler.php
@@ -2,52 +2,26 @@
 
 namespace ITPalert\Web2sms;
 
-use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
-
-use ITPalert\Web2sms\Exceptions\Request;
-use ITPalert\Web2sms\Exceptions\Server;
 use ITPalert\Web2sms\Exceptions\Exception;
 
 class APIExceptionHandler
 {
-    /**
-     * Format to use for the rfc7807 formatted errors
-     *
-     * @var string
-     */
-    protected $rfc7807Format = "%s: %s. See %s for more information";
-
-    /**
-     * @throws Exception\Exception
-     *
-     * @return Exception\Request|Exception\Server
-     */
     public function __invoke(ResponseInterface $response)
     {
-        $pos = strpos($response->getBody(), '{"error":{');
-        $body = json_decode(substr($response->getBody(), $pos));
-
+        $status = $response->getStatusCode();
+        $body = (string) $response->getBody();
         $response->getBody()->rewind();
 
-        // This message isn't very useful, but we shouldn't ever see it
-        $errorTitle = 'Unexpected error';
+        $data = json_decode($body, true);
+        $errorMessage = $data['error']['message'] ?? $body;
+        
+        $message = sprintf(
+            'Web2SMS API Error [%d]: %s',
+            $status,
+            $errorMessage
+        );
 
-        if (isset($body->error)) {
-            $errorTitle = $body->error->message;
-        }
-
-        $status = (int)$response->getStatusCode();
-
-        if ($status >= 400 && $status < 500) {
-            $e = new Request($errorTitle, $status);
-        } elseif ($status >= 500 && $status < 600) {
-            $e = new Server($errorTitle, $status);
-        } else {
-            $e = new Exception('Unexpected HTTP Status Code');
-            throw $e;
-        }
-
-        return $e;
+        return new Exception($message, $status);
     }
 }
--- src/Exceptions/Server.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ITPalert\Web2sms\Exceptions;
-
-class Server extends Exception
-{
-}
